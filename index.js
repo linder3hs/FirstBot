ca1eec7a89ca0c6a2afd4a1f02d4b8de6c440256
linder3hs <linderhassinger00@gmail.com>
2018-03-01 15:23:22 -0500

    Firs commit
    First Commit

---
+++ index.js
@@ -0,0 +1,176 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+
+const tokenF = process.env.FB_VERIFY_TOKEN
+const access = process.env.FB_ACCESS_TOKEN
+
+app.set('port', (process.env.PORT || 5000))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+  res.send("Hola amigos :v  soy un chatbot")
+})
+
+//let token = "EAALZAWNaM1jIBAHpmhydvUNMGE3ZAsv9jZBhlXGZBDqOPEPdZACWZANxW6tniJd4MOiGROOUwnVzIGXDJ4elnf5ndeZAM0gPEzUlr3zWhlWg9T8JbZBa71kV3pe3MKBQ4ncF61cOXa4dAjyri5gdvmDDLXdMfHKwpXjB8zZA4OY18AgZDZD"
+//Facebook
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === tokenF) {
+    res.send(req.query['hub.challenge'])
+    res.send("Rigth token")
+
+  }
+    res.send("Wrong token")
+})
+
+app.post('/webhook', (req, res) => {
+
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+  // Gets the body of the webhook event
+  let webhook_event = entry.messaging[0];
+  console.log(webhook_event);
+
+
+  // Get the sender PSID
+  let sender_psid = webhook_event.sender.id;
+  console.log('Sender PSID: ' + sender_psid);
+
+  // Check if the event is a message or postback and
+  // pass the event to the appropriate handler function
+  if (webhook_event.message) {
+    handleMessage(sender_psid, webhook_event.message);
+  } else if (webhook_event.postback) {
+    handlePostback(sender_psid, webhook_event.postback);
+  }
+
+});
+
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+   // Checks if the message contains text
+   if (received_message.text) {
+     // Create the payload for a basic text message, which
+     // will be added to the body of our request to the Send API
+     response = {
+       "text": `Tu enviaste este mensaje: "${received_message.text}". ahora envia una imagen!`
+     }
+   } else if (received_message.attachments) {
+     // Get the URL of the message attachment
+     let attachment_url = received_message.attachments[0].payload.url;
+     response = {
+       "attachment": {
+         "type": "template",
+         "payload": {
+           "template_type": "generic",
+           "elements": [{
+             "title": "Esta es la imagen que mandaste?",
+             "subtitle": "Toca el boton para responder.",
+             "image_url": attachment_url,
+             "buttons": [
+               {
+                 "type": "postback",
+                 "title": "Si!",
+                 "payload": "yes",
+               },
+               {
+                 "type": "postback",
+                 "title": "No!",
+                 "payload": "no",
+               },
+               {
+                 "type": "postback",
+                 "title": "Puede ser!",
+                 "payload": "ojala",
+               }
+             ],
+           }]
+         }
+       }
+     }
+   }
+
+   // Send the response message
+   callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+}
+
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": access },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+}
+
+function handlePostback(sender_psid, received_postback) {
+  let response;
+
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Gracias todo correcto!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, intente con otra imagen ;)." }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
+
+app.listen(app.get('port'), function() {
+  console.log(app.get('port'));
+})
